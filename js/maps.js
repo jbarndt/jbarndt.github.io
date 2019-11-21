7f606d39c3261f724d8bed4a02c254d1e8ed151e
Jonathan Barndt <jbarndt@cs.unm.edu>
2019-11-21 10:19:19 -0700

    added coordinate finding block

--- js/maps.js
+++ js/maps.js
@@ -16,11 +16,7 @@
 
   var EARTH_RADIUS = 6371000; //meters
 
-  //Radius in km to determine if the ISS is overhead
-  var OVERHEAD_DIST = 500
-
   var locations = {};
-  var issData = null;
 
   function getLocation(str, callback) {
 
@@ -50,99 +46,41 @@
     });
   }
 
-  function updateISSLocation() {
-    $.ajax({
-      type: "GET",
-      dataType: "json",
-      //url: "http://api.open-notify.org/iss-now.json",
-      url: "https://api.wheretheiss.at/v1/satellites/25544",
-      success: function(data) {
-        issData = data;
-      },
-      error: function(jqxhr, textStatus, error) {
-        console.log("Error downloading ISS data");
-      }
-    });
-  }
-
-  function distanceBetween(lat, lon, unit) {
-    var lat1 = lat * (Math.PI/180);
-    var lat2 = issData.latitude * (Math.PI/180);
-    var d1 = (issData.latitude - lat) * (Math.PI/180);
-    var d2 = (issData.longitude - lon) * (Math.PI/180);
-
-    var a = Math.sin(d1/2) * Math.sin(d1/2) +
-      Math.cos(lat1) * Math.cos(lat2) *
-      Math.sin(d2/2) * Math.sin(d2/2);
-    var c = 2 * Math.atan2(Math.sqrt(a), Math.sqrt(1-a));
-    var dist = EARTH_RADIUS * c;
-
-    if (unit === "miles") dist *= 0.00062137;
-    else dist /= 1000;
-
-    return +(Math.round(dist + "e+2") + "e-2");
-  }
-
-  ext.whenISSPasses = function(str) {
-    if (!issData) return;
+  ext.getloc = function(str, coords) {
 
     getLocation(str, function(loc) {
-      var dist = distanceBetween(loc.coords[1], loc.coords[0], "kilometers");
-      if (dist <= OVERHEAD_DIST) loc.overhead = true;
-      else loc.overhead = false;
+      var lat = loc.coords[1];
+	  var lon = loc.coords[0];
     });
 
-    if (!locations[str])
-      return false;
-
-    return locations[str].overhead;
-  };
-
-  ext.distanceFrom = function(str, unit, callback) {
-    getLocation(str, function(loc) {
-      var dist = distanceBetween(loc.coords[1], loc.coords[0], unit);
-      if (dist <= OVERHEAD_DIST) loc.overhead = true;
-      else loc.overhead = false;
-      callback(dist);
-      return;
-    });
-  };
-
-  ext.getISSInfo = function(stat) {
-    if (!issData) return;
-    if (stat === "longitude" || stat === "latitude")
-      return issData[stat].toFixed(6).toString();
-    else
-      return issData[stat].toFixed(2).toString();
+    if (coords === "longitude")
+      return lon;
+    else if (coords === "latitude")
+      return lat;
   };
 
   ext._getStatus = function() {
     return { status:2, msg:'Ready' };
   };
 
-  ext._shutdown = function() {
-    if (poller) {
-      clearInterval(poller);
-      poller = null;
-    }
-  };
+  // Cleanup function when the extension is unloaded
+  ext._shutdown = function() {};
 
   var descriptor = {
     blocks: [
-      ['h', 'when ISS passes over %s', 'whenISSPasses', 'Boston, MA'],
-      ['R', 'distance from %s in %m.measurements', 'distanceFrom', 'Boston, MA', 'kilometers'],
-      ['r', 'current ISS %m.loc', 'getISSInfo', 'longitude']
+      //['h', 'when ISS passes over %s', 'whenISSPasses', 'Boston, MA'],
+      //['R', 'distance from %s in %m.measurements', 'distanceFrom', 'Boston, MA', 'kilometers'],
+      //['r', 'current ISS %m.loc', 'getISSInfo', 'longitude']
+
+	  ['R', 'location of %s in %m.loc', 'getloc', 'Boston, MA', 'longitude']
     ],
     menus: {
-      loc: ['longitude', 'latitude', 'altitude', 'velocity'],
-      measurements: ['kilometers', 'miles']
+      loc: ['longitude', 'latitude'],
+      //measurements: ['kilometers', 'miles']
     },
-    url: 'https://raw.githubusercontent.com/jbarndt/scratch_maps/master/js/maps.js'
+    url: 'https://jbarndt.github.io/js/maps.js'
   };
 
   ScratchExtensions.register('Maps', descriptor, ext);
-
-  updateISSLocation();
-  var poller = setInterval(updateISSLocation, 2000);
 
 })({});
